578cc60c45f5e180e1ca7c988f0d0206d61f73c7
Daniel Spangenberg <daniel@cloudquery.io>
2021-08-19 17:13:25 +0200

    Bump version

--- resources/provider.go
+++ resources/provider.go
@@ -19,7 +19,7 @@ var migrations embed.FS
 func Provider() *provider.Provider {
 	return &provider.Provider{
 		Name:       "test",
-		Version:    "v0.0.5",
+		Version:    "v0.0.6",
 		Migrations: migrations,
 		Configure: func(logger hclog.Logger, i interface{}) (schema.ClientMeta, error) {
 			return &client.TestClient{L: logger}, nil
